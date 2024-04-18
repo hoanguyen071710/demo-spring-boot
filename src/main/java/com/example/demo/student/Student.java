41ded423fbb93ed62675ea16c9b3f01305b92639
hoanguyen071710 <hoanguyen071710@gmail.com>
2024-04-18 21:12:36 +0700

    Fix lib error @Entity and @Table etc to Jakarta

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -2,12 +2,31 @@ package com.example.demo.student;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.GenerationType;
+
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
+@Table
 @Getter
 @Setter
 public class Student {
+    @Id
+    @SequenceGenerator(
+        name = "student_sequence",
+        sequenceName = "student_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
